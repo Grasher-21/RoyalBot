eb112a6ed657358d59159a328c3180e5846010ca
Grasher <guilherme.jmcc.gomes@gmail.com>
2020-08-06 00:48:27 +0100

    improved code readability and functionality

--- bot.js
+++ bot.js
@@ -14,8 +14,6 @@ const elryusMessageId = '740225386050224249';
 
 client.login(process.env.BOT_TOKEN);
 
-// client.on('ready', () => { console.log(`Logged in as ${client.user.tag}!`); });
-
 client.on('message', message => {
    // properties of the message
    // console.log(message); 
@@ -28,86 +26,90 @@ client.on('message', message => {
 
       var command = message.content.split(' ');
 
-      // Removing loot
-      if (command.length == 2) {
-         if (command[0] === '!del' && command[1] >= 1 && command[1] <= 14) {
-            message.channel.messages.fetch(elryusMessageId).then(msg => {
-               var msgArray = msg.content.split('\n');
+      switch (command[0]) {
+         case '!del': // Removing loot
+            if (command.length == 2 && command[1] >= 1 && command[1] <= 14) {
+               message.channel.messages.fetch(elryusMessageId).then(msg => {
+                  var msgArray = msg.content.split('\n');
 
-               if (command[1] >= 1 && command[1] <= 9) {
-                  msgArray[command[1]] = `#${command[1]}  = Empty`;
-               }
-               else {
-                  msgArray[command[1]] = `#${command[1]} = Empty`;
-               }
+                  if (command[1] >= 1 && command[1] <= 9) {
+                     msgArray[command[1]] = `#${command[1]}  = Empty`;
+                  }
+                  else {
+                     msgArray[command[1]] = `#${command[1]} = Empty`;
+                  }
 
-               var newMessage = '';
-               for (var i = 0; i < msgArray.length; i++) {
-                  newMessage += msgArray[i] + '\n';
-               }
+                  var newMessage = '';
+                  for (var i = 0; i < msgArray.length; i++) {
+                     newMessage += msgArray[i] + '\n';
+                  }
 
-               msg.edit(newMessage);
-            });
-         }
-         else {
-            invalidCommand(message);
-         }
+                  msg.edit(newMessage);
+
+                  notifySuccessRequest(message);
+               });
+            }
+            else {
+               invalidCommand(message);
+            }
+            message.delete();
+            break;
+
+         case '!add': // Adding loot
+            if (command.length >= 3 && command[1] >= 1 && command[1] <= 14) {
+               message.channel.messages.fetch(elryusMessageId).then(msg => {
+                  var msgArray = msg.content.split('\n');
+
+                  if (command[1] >= 1 && command[1] <= 9) {
+                     msgArray[command[1]] = `#${command[1]}  = `;
+                  }
+                  else {
+                     msgArray[command[1]] = `#${command[1]} = `;
+                  }
+
+                  for (var i = 2; i < command.length; i++) {
+                     msgArray[command[1]] += command[i] + ' ';
+                  }
+
+                  var newMessage = '';
+                  for (var i = 0; i < msgArray.length; i++) {
+                     newMessage += msgArray[i] + '\n';
+                  }
+
+                  msg.edit(newMessage);
+
+                  notifySuccessRequest(message);
+               });
+            }
+            else {
+               invalidCommand(message);
+            }
+            break;
       }
-      // Adding loot
-      else if (command.length >= 3) {
-         if (command[0] === '!add' && command[1] >= 1 && command[1] <= 14) {
-            message.channel.messages.fetch(elryusMessageId).then(msg => {
-               var msgArray = msg.content.split('\n');
-
-               if (command[1] >= 1 && command[1] <= 9) {
-                  msgArray[command[1]] = `#${command[1]}  = `;
-               }
-               else {
-                  msgArray[command[1]] = `#${command[1]} = `;
-               }
-
-               for (var i = 2; i < command.length; i++) {
-                  msgArray[command[1]] += command[i] + ' ';
-               }
-
-               var newMessage = '';
-               for (var i = 0; i < msgArray.length; i++) {
-                  newMessage += msgArray[i] + '\n';
-               }
-
-               msg.edit(newMessage);
-            });
-         }
-         else {
-            invalidCommand(message);
-         }
-      }
-      else {
-         invalidCommand(message);
-      }
-
-      message.delete();
    }
    else if (message.channel.id == generalChannelId && message.content.startsWith(botCommand) && message.author.id != botId) {
       logCommandRequest(message);
 
       var command = message.content.split(' ');
 
-      // Generate random number
-      if (command.length == 2) {
-         if (command[0] === '!roll' && !isNaN(command[1]) && command[1] > 0) {
-            var number = Math.round((Math.random() * command[1]) + 1);
-            message.reply(`number rolled: ${number}`);
-         }
-         else {
-            invalidCommand(message);
-         }
-      }
-      else {
-         invalidCommand(message);
+      switch (command[0]) {
+         case '!roll':
+            if (command.length == 2 && !isNaN(command[1]) && command[1] > 0) {
+               var number = Math.round((Math.random() * command[1]) + 1);
+               message.reply(`number rolled: ${number}`);
+            }
+            break;
       }
    }
 });
+
+function notifySuccessRequest(message) {
+   message.reply('command executed successfully').then(msg => {
+      msg.delete({ timeout: 5000 });
+   });
+
+   message.delete({ timeout: 5000 });
+}
 
 function logCommandRequest(message) {
    client.channels.cache.get(botLogChannelId).send(`\`\`\`
@@ -121,6 +123,8 @@ function invalidCommand(message) {
    message.reply('Invalid command!').then(msg => {
       msg.delete({ timeout: 5000 });
    });
+
+   message.delete({ timeout: 5000 });
 }
 
 function deleteMessagesFromChannel(message) {
