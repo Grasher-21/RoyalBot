ac1cad8a8c89673cfd5a6ab2ef4f9853cad2bedc
Grasher <guilherme.jmcc.gomes@gmail.com>
2020-08-05 12:32:53 +0100

    Added log commands

--- bot.js
+++ bot.js
@@ -1,10 +1,12 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const botId = 730084929693810691;
+const botId = '730084929693810691';
+const botLogChannelId = '740500341346402306';
 const botCommand = '!';
 
-const userAdmin = 101462082016260096;
+const userAdmin = '101462082016260096';
+const elryusChannelId = '729854794986160168';
 const elryusMessageId = '740225386050224249';
 
 client.login(process.env.BOT_TOKEN);
@@ -12,93 +14,100 @@ client.login(process.env.BOT_TOKEN);
 client.on('ready', () => { console.log(`Logged in as ${client.user.tag}!`); });
 
 client.on('message', message => {
-	// console.log(message); // properties of the message
-	
-	if (message.channel.id == 729854794986160168) // Elryus channel
-	// if (message.channel.id == 735441393090035835) // Bot Channel
-	{
-		// User Admin rights
-		// if (message.author.id == userAdmin &&
-			// message.content === `!del messages`)
-		// {
-			// console.log('Deleting messages...');
-			// async function clear() {
-				// message.channel.bulkDelete(100).catch(console.error);
-			// }
-			// clear();
-			// console.log('Messages deleted...');
-		// }
-		
-		if (message.content.startsWith(botCommand) &&
-			message.author.id != botId)
-		{
-			var command = message.content.split(' ');
-			
-			// Removing loot
-			if (command.length == 2) 
-			{
-				if (command[0] === '!del' && command[1] >= 1 && command[1] <= 14)
-				{
-					message.channel.messages.fetch(elryusMessageId)
-						.then(msg => {
-							var msgArray = msg.content.split('\n');
-							
-							if (command[1] >= 1 && command[1] <= 9)
-							{
-								msgArray[command[1]] = `#${command[1]}  = Empty`;
-							}
-							else
-							{
-								msgArray[command[1]] = `#${command[1]} = Empty`;
-							}
-							
-							var newMessage = '';
-							for(var i = 0; i < msgArray.length; i++)
-							{
-								newMessage += msgArray[i] + '\n';
-							}
-							
-							msg.edit(newMessage);
-						});
-					
-					message.delete();
-				}
-			}
-			// Adding loot
-			else if (command.length >= 3) 
-			{
-				if (command[0] === '!add' && command[1] >= 1 && command[1] <= 14)
-				{
-					message.channel.messages.fetch(elryusMessageId)
-						.then(msg => {
-							var msgArray = msg.content.split('\n');
-							
-							if (command[1] >= 1 && command[1] <= 9)
-							{
-								msgArray[command[1]] = `#${command[1]}  = `;
-							}
-							else
-							{
-								msgArray[command[1]] = `#${command[1]} = `;
-							}
-							
-							for(var i = 2; i < command.length; i++)
-							{
-								msgArray[command[1]] += command[i] + ' ';
-							}
-							
-							var newMessage = '';
-							for(var i = 0; i < msgArray.length; i++)
-							{
-								newMessage += msgArray[i] + '\n';
-							}
-							
-							msg.edit(newMessage);
-						});
-					
-					message.delete();
-				}
-			}	
-		}
-	}
+   // properties of the message
+   // console.log(message); 
+
+   if (message.channel.id == elryusChannelId && message.content.startsWith(botCommand) && message.author.id != botId) {
+      logCommandRequest(message);
+
+      var command = message.content.split(' ');
+
+      // Removing loot
+      if (command.length == 2) {
+         if (command[0] === '!del' && command[1] >= 1 && command[1] <= 14) {
+            message.channel.messages.fetch(elryusMessageId).then(msg => {
+               var msgArray = msg.content.split('\n');
+
+               if (command[1] >= 1 && command[1] <= 9) {
+                  msgArray[command[1]] = `#${command[1]}  = Empty`;
+               }
+               else {
+                  msgArray[command[1]] = `#${command[1]} = Empty`;
+               }
+
+               var newMessage = '';
+               for (var i = 0; i < msgArray.length; i++) {
+                  newMessage += msgArray[i] + '\n';
+               }
+
+               msg.edit(newMessage);
+            });
+
+            message.delete();
+         }
+      }
+      // Adding loot
+      else if (command.length >= 3) {
+         if (command[0] === '!add' && command[1] >= 1 && command[1] <= 14) {
+            message.channel.messages.fetch(elryusMessageId).then(msg => {
+               var msgArray = msg.content.split('\n');
+
+               if (command[1] >= 1 && command[1] <= 9) {
+                  msgArray[command[1]] = `#${command[1]}  = `;
+               }
+               else {
+                  msgArray[command[1]] = `#${command[1]} = `;
+               }
+
+               for (var i = 2; i < command.length; i++) {
+                  msgArray[command[1]] += command[i] + ' ';
+               }
+
+               var newMessage = '';
+               for (var i = 0; i < msgArray.length; i++) {
+                  newMessage += msgArray[i] + '\n';
+               }
+
+               msg.edit(newMessage);
+            });
+
+            message.delete();
+         }
+      }
+   }
 });
+
+function logCommandRequest(message) {
+   client.channels.cache.get(botLogChannelId).send(`${message.author.username} requested: ${message.content}`);
+}
+
+
+//if (message.channel.id == 735441393090035835) // Bot Channel
+//{
+//   // User Admin rights
+//   if (message.author.id == userAdmin && message.content === `!del messages`) {
+//      console.log('Deleting messages...');
+//      async function clear() {
+//         message.channel.bulkDelete(100).catch(console.error);
+//      }
+//      clear();
+//      console.log('Messages deleted...');
+//   }
+//}
+
+var elryusInitialMessage = `\`\`\`Loot in Elryus' Island chests
+#1  = Empty
+#2  = Empty
+#3  = Empty
+#4  = Empty
+#5  = Empty
+#6  = Empty
+#7  = Empty
+#8  = Empty
+#9  = Empty
+#10 = Empty
+#11 = Empty
+#12 = Empty
+#13 = Empty
+#14 = Empty
+\`\`\``;
