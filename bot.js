e72a1065c78e422069b37e42bb48594c5e7d3c8c
Grasher <guilherme.jmcc.gomes@gmail.com>
2020-08-07 02:01:57 +0100

    changed the client.on(ready) to send message to the log channel instead of the test channel

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ const elryusMessageId = '740225386050224249';
 client.login(process.env.BOT_TOKEN);
 
 client.on('ready', () => {
-   client.channels.cache.get(botTestChannelId).send('*is back online*');
+   client.channels.cache.get(botLogChannelId).send('*is back online*');
 });
 
 client.on('message', message => {
